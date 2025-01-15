e414d690f10379651248dd4b286c4e0215a856a8
jinhongkuan <kuanjh123@gmail.com>
2025-01-15 03:03:28 -0500

    Revert "draft linkedin"
    This reverts commit ff1b74fda06c2651c18ba62775cf6541f9c88756.

--- src/lib/algo/schema.ts
+++ src/lib/algo/schema.ts
@@ -1,34 +1,25 @@
 import { GetUserProfileResponse, RepositoriesResponse } from '$lib/github/schema';
-import { LinkedInProfile } from '$lib/linkedin/schema';
 import { z } from 'zod';
 
 export type QueryTaskResult = {
     batchSize: number;
-    status: 'PENDING' | 'COMPLETED' | 'CANCELLED';
+    status: 'PENDING' | 'COMPLETED';
 }
 
-export const GithubContributorsMatchQueryMetadata = z.object({
+export const ContributorsMatchQueryMetadata = z.object({
     keywordsString: z.string(),
     role: z.array(z.string()),
     language: z.array(z.string()).optional(),
     location: z.array(z.string()).optional(),
 });
 
-export type GithubContributorsMatchQueryMetadata = z.infer<typeof GithubContributorsMatchQueryMetadata>;
+export type ContributorsMatchQueryMetadata = z.infer<typeof ContributorsMatchQueryMetadata>;
 
-export const LinkedinProfilesMatchQueryMetadata = z.object({
-    query: z.string(),
-});
-
-export type LinkedinProfilesMatchQueryMetadata = z.infer<typeof LinkedinProfilesMatchQueryMetadata>;
-
-export const OpenAIQueryMatchingUsersResponse = GithubContributorsMatchQueryMetadata
+export const OpenAIQueryMatchingUsersResponse = ContributorsMatchQueryMetadata
 export const OpenAIQueryMatchingUsersSummaryResponse = z.object({
     summary: z.string(),
     rating: z.number(),
 });
-
-// --- Github Contributors Match Task ---
 export const ContributorsMatchTaskCfg = z.object({
     batchSize: z.number(),
     minContributions: z.number(),
@@ -50,31 +41,12 @@ export const ContributorsMatchTaskState = z.object({
         total_contributions: z.number(),
         repositories: RepositoriesResponse,
     })),
-}).extend(GithubContributorsMatchQueryMetadata.shape);
+}).extend(ContributorsMatchQueryMetadata.shape);
+
+
 export type ContributorsMatchTaskCfg = z.infer<typeof ContributorsMatchTaskCfg>;
 
-// --- Linkedin Contributors Match Task ---
-export const LinkedinProfilesMatchTaskCfg = z.object({
-    maxProfiles: z.number(),
-    maxCompanies: z.number(),
-});
-
-export const LinkedinProfilesMatchTaskState = z.object({
-    type: z.literal('linkedin_profiles_match'),
-    config: LinkedinProfilesMatchTaskCfg,
-    page: z.number(),
-    query: z.string(),
-    profiles: z.record(z.string(), LinkedInProfile),
-    companies: z.object({
-        companyUrls: z.array(z.string()),
-        index: z.number(),
-    }),
-});
-export type LinkedinProfilesMatchTaskState = z.infer<typeof LinkedinProfilesMatchTaskState>;
-
-
-export type LinkedinProfilesMatchTaskCfg = z.infer<typeof LinkedinProfilesMatchTaskCfg>;
-export const QueryTaskState = z.discriminatedUnion('type', [ContributorsMatchTaskState, LinkedinProfilesMatchTaskState]);
+export const QueryTaskState = z.discriminatedUnion('type', [ContributorsMatchTaskState]);
 
 export type OpenAIQueryMatchingUsersResponse = z.infer<typeof OpenAIQueryMatchingUsersResponse>;
 export type OpenAIQueryMatchingUsersSummaryResponse = z.infer<typeof OpenAIQueryMatchingUsersSummaryResponse>;
